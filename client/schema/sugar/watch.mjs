db1095e85ed813c2edb07f50b659a5193303e618
Derryk Boyd <derryk@redmantech.com>
2025-12-18 16:03:42 -0700

    convert some missed zod3 syntax

--- client/schema/sugar/watch.mjs
+++ client/schema/sugar/watch.mjs
@@ -1,38 +1,22 @@
+// @ts-check
 import { z } from 'zod'
 import { CouchConfig } from '../config.mjs'
 
 const WatchEmitter = z.object({
-  on: z.function()
-    .args(z.string(), z.function().args(z.any()).returns(z.void()))
-    .returns(z.any()),
-  removeListener: z.function()
-    .args(z.string(), z.function().args(z.any()).returns(z.void()))
-    .returns(z.any()),
-  stop: z.function().returns(z.void())
+  on: z.function({ input: [z.string(), z.function()], output: z.any() }),
+  removeListener: z.function({ input: [z.string(), z.function()], output: z.any() }),
+  stop: z.function({ output: z.void() })
 })
 
 export const WatchOptions = z.object({
   include_docs: z.boolean().prefault(false)
 }).partial()
 
-export const WatchDocs = z.function()
-  .args(
-    CouchConfig,
-    z.union([z.string(), z.array(z.string())]),
-    z.function().args(z.any()).returns(z.void()),
-    WatchOptions
-  )
-  .returns(WatchEmitter)
+export const WatchDocs = z.function({ input: [CouchConfig, z.union([z.string(), z.array(z.string())]), z.function(), WatchOptions], output: WatchEmitter })
 
 /** @typedef { z.infer<typeof WatchOptions> } WatchOptionsSchema */
 /** @typedef { z.infer<typeof WatchDocs> } WatchDocsSchema */
 
-export const WatchDocsBound = z.function()
-  .args(
-    z.union([z.string(), z.array(z.string())]),
-    z.function().args(z.any()).returns(z.void()),
-    WatchOptions
-  )
-  .returns(WatchEmitter)
+export const WatchDocsBound = z.function({ input: [z.union([z.string(), z.array(z.string())]), z.function(), WatchOptions], output: WatchEmitter })
 
 /** @typedef { z.infer<typeof WatchDocsBound> } WatchDocsBoundSchema */
